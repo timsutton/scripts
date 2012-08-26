d2c718b89ade25ef49e7003c5a3748fd1215d3a0
Timothy Sutton <tim@synthist.net>
2012-08-26 15:23:01 -0400

    Updating make_k2client_custom_package.py to use new flat pkg format and modify the plist directly.

--- k2client_custom_package_builder/make_k2client_custom_package.py
+++ k2client_custom_package_builder/make_k2client_custom_package.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python
 
-# Grab the most recent K2 Client release, and configure it for an organization's deployment
-# environment.
+# Sassafras K2Client package grabber/customizer
 
-# k2ConfigOpts should be modified to suit. For a full help on the available options, run
-# the k2clientconfig script located in Contents/Resources of the K2 Client installer.
+# Grabs the most recent K2 Client release, and configure it for an organization's deployment
+# environment. An optional argument is the path to output the pkg to, or else the current
+# working directory will be used.
 
-# The sample options here specify the host (and -g yes to overwrite the existing host),
-# a silent install, and for the KeyAccess prefpane to be locked for users.
+# plist_opts below should be modified to suit. These correspond to values that would be set in
+# KeyAccess.pkg's 'k2clientconfig.plist' file. The k2clientconfig script located in
+# Contents/Resources of versions prior to 7.0.10 of the K2 Client installer documents the full
+# set of options. The options used here are the equivalent to running k2clientconfig with
+# the following options:
+# k2ConfigOpts = ['-h', 'org.example.k2client', '-g', 'yes', '-s', '2', '-l', 'yes']
 
-# configure these for your org:
-k2ConfigOpts = ['-h', 'org.example.k2server', '-g', 'yes', '-s', '2', '-l', 'yes']
+plist_opts = {
+    'KSAddress': 'org.example.k2client',    # KeyServer address
+    'AddressPromptUser': '0',       # address prompting during installation
+    'AddressDisableChange': '1',    # address change allowed during installation
+    'AddressDefaultCurrent': '0',   # use default address (if '0', override address with one used here)
+    'KASettingsLocked': '1',        # lock the preference pane
+}
 
 import os
 import subprocess
@@ -18,46 +27,47 @@ import sys
 import urllib
 import plistlib
 import shutil
+import tempfile
+import xml.etree.ElementTree as ET
 
-scriptDir = os.path.dirname(sys.argv[0])
-distVolume = '/Volumes/K2Client'		# making assumptions about the volume title...
-K2ClientURL = 'http://www.sassafras.com/links/K2Client.dmg'
-distDmgDownloadPath = os.path.join(scriptDir, 'K2Client.dmg')
+pkgutil = '/usr/sbin/pkgutil'
+tmpDownloadDir = tempfile.mkdtemp()
+K2ClientURL = 'http://www.sassafras.com/links/K2Client.pkg'
+distPkgDownloadPath = os.path.join(tmpDownloadDir, 'K2Client.pkg')
 
-k2ConfigCmd = 'k2clientconfig'
-k2ConfigPath = os.path.join(scriptDir, 'K2Client.mpkg', 'Contents', 'Resources', k2ConfigCmd)
-k2ConfigFull = [k2ConfigPath] + k2ConfigOpts
-k2ConfigDebug = [k2ConfigPath] + ['-d']
+print 'Downloading K2Client from URL %s' % K2ClientURL
+urllib.urlretrieve(K2ClientURL, distPkgDownloadPath)
 
-print 'Downloading K2Client...'
-urllib.urlretrieve(K2ClientURL, os.path.join(scriptDir, distDmgDownloadPath))
-print 'Mounting dmg...'
-subprocess.call(['/usr/bin/hdiutil', 'attach', '-nobrowse', '-quiet', distDmgDownloadPath])
-print 'Copying K2Client.mpkg...'
-if os.path.exists(distVolume):
-    # being lazy and using cp to ensure copying potential resource forks/xattrs
-    subprocess.call(['cp', '-R', '/Volumes/K2Client/K2Client.mpkg', scriptDir])
-else:
+print "Downloaded flat pkg is at %s." % distPkgDownloadPath
+expandedPath = os.path.join(tempfile.mkdtemp(), 'pkg')
+print "Expanding to %s..." % expandedPath
+exp_retcode = subprocess.call([pkgutil, '--expand', distPkgDownloadPath, expandedPath], shell=False)
+if exp_retcode:
+    print "There was an error expanding the flat pkg. Check whether the .pkg file is valid."
     sys.exit(1)
-print 'Unmounting DMG...'
-subprocess.call(['/usr/bin/hdiutil', 'detach', distVolume])
 
-k2Info = plistlib.readPlist(os.path.join(scriptDir, 'K2Client.mpkg/Contents/Info.plist'))
-k2Version = k2Info['CFBundleVersion']
+clientPkginfo = ET.parse(os.path.join(expandedPath, 'KeyAccess.pkg/PackageInfo'))
+k2Version = clientPkginfo.getroot().attrib['version']
 print 'Version downloaded is %s' % k2Version
-print 'Configuring package with custom options...'
-subprocess.call(k2ConfigFull)
-print 'Displaying k2clientconfig config...'
-subprocess.call(k2ConfigDebug)
 
-k2VersionedFolder = 'K2Client-%s' % k2Version
-k2VersionedDmg = k2VersionedFolder + '.dmg'
-os.mkdir(os.path.join(scriptDir, k2VersionedFolder))
+print 'Configuring package with custom options:'
+print plist_opts
+clientconfigPlistPath = os.path.join(expandedPath, 'KeyAccess.pkg/Scripts/k2clientconfig.plist')
+clientconfigPlist = plistlib.readPlist(clientconfigPlistPath)
+for (k, v) in plist_opts.items():
+    clientconfigPlist[k] = v
+plistlib.writePlist(clientconfigPlist, clientconfigPlistPath)
 
-print 'Wrapping package into %s...' % k2VersionedDmg
-shutil.move(os.path.join(scriptDir, 'K2Client.mpkg'), os.path.join(scriptDir, k2VersionedFolder))
-subprocess.call(['/usr/bin/hdiutil', 'create', os.path.join(scriptDir, k2VersionedDmg), '-srcfolder', os.path.join(scriptDir, k2VersionedFolder)])
-print "Cleaning up..."
-shutil.rmtree(os.path.join(scriptDir, k2VersionedFolder))
-os.remove(os.path.join(scriptDir, 'K2Client.dmg'))
-print "Done."
+k2VersionedPkg = 'K2Client-%s.pkg' % k2Version
+
+if len(sys.argv) > 1 and os.path.isdir(sys.argv[1]):
+    pkgOutputPath = os.path.join(sys.argv[1], k2VersionedPkg)
+else:
+    pkgOutputPath = os.path.join(os.getcwd(), k2VersionedPkg)
+
+subprocess.call([pkgutil, '--flatten', expandedPath, pkgOutputPath])
+
+print "Cleaning up temp folders..."
+shutil.rmtree(tmpDownloadDir)
+shutil.rmtree(expandedPath)
+print "Done. Finished package written to %s." % pkgOutputPath
